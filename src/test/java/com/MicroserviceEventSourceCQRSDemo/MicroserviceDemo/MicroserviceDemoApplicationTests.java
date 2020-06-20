d0f0ac03e6bc9f3db90eea051d5523c38fb21d12
Jonas Maibohm Poulsen <jona6321@edu.eal.dk>
2020-06-20 21:07:14 +0200

    added test framework, changed deployment and service yaml

--- src/test/java/com/MicroserviceEventSourceCQRSDemo/MicroserviceDemo/MicroserviceDemoApplicationTests.java
+++ src/test/java/com/MicroserviceEventSourceCQRSDemo/MicroserviceDemo/MicroserviceDemoApplicationTests.java
@@ -1,13 +1,13 @@
-//package com.MicroserviceEventSourceCQRSDemo.MicroserviceDemo;
-//
-//import org.junit.jupiter.api.Test;
-//import org.springframework.boot.test.context.SpringBootTest;
-//
-//@SpringBootTest
-//class MicroserviceDemoApplicationTests {
-//
-//	@Test
-//	void contextLoads() {
-//	}
-//
-//}
+package com.MicroserviceEventSourceCQRSDemo.MicroserviceDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
